047dd9a031573f47105b6561fe8d1bf0a818b412
Otabek0111 <134247761+Otabek0111@users.noreply.github.com>
2023-10-31 16:27:25 -0400

    webpack updated

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -69,8 +69,8 @@ module.exports = () => {
             loader: 'babel-loader',
             options: { 
               presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-class-properties']
-          },
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],          
+            },
           },
         }
       ],
